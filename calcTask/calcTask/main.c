3b09f1a5f7d07ac9eba5b4d0152d04c2d0149c7a
ksenia <ksenia@mail.ru>
2015-06-14 11:30:59 +0500

    Add timing for read/write operations

--- calcTask/calcTask/main.c
+++ calcTask/calcTask/main.c
@@ -3,7 +3,32 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 #define NAME_LENGTH 50
+#define LOOPS 1000
+// #define timing(a) start=clock(); a; diff = clock() - start; msec = diff * 1000 / CLOCKS_PER_SEC; printf("msecs: %d\n",msec);
+
+void writetest(char* name, char* buffer) {
+    for (int i=0; i < LOOPS; ++i) {
+        FILE* out = fopen(name, "ab+");
+        if (out == NULL) {
+            return;
+        }
+        fwrite(buffer,sizeof(char), sizeof(buffer), out);
+        fclose(out);
+    }
+}
+
+void readtest(char* name, char* buffer) {
+    for (int i=0; i < LOOPS; ++i) {
+        FILE* in = fopen(name, "rb");
+        if (in == NULL) {
+            return;
+        }
+        fread(buffer,sizeof(char), sizeof(buffer), in);
+        fclose(in);
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -35,6 +60,28 @@ int main(int argc, char *argv[])
     fclose(in);
     fclose(out);
     int ret = rename(file, argv[2]);            // what if can`t?
+
+    // Read/Write tests
+    char name[50];
+    strcpy(name, argv[2]);
+    strcat(name, ".rwtest");
+    char* buffer = (char*) malloc(sizeof(char) * 1000);;
+    for(int i=0; i<1000; ++i)
+        buffer[i]=(char)i;
+
+    clock_t start = clock(), diff;
+    writetest(name,buffer);
+    diff = clock() - start;
+    int msec = diff * 1000 / CLOCKS_PER_SEC;
+    printf("Time taken for write %d seconds %d milliseconds\n", msec/1000, msec%1000);
+
+    start = clock();
+    readtest(name,buffer);
+    diff = clock() - start;
+    msec = diff * 1000 / CLOCKS_PER_SEC;
+    printf("Time taken for read %d seconds %d milliseconds\n", msec/1000, msec%1000);
+
+    free(buffer);
     return 0;
 }
 
